22686728ed6bea63f3ca624bba79f97953b3b042
92fumi <fu.yajima@kanetsu-sw.co.jp>
2025-06-17 17:07:03 +0900

    Add files via upload
    setKariPasswordメソッドを実装しました。今後、メソッド項番4, updatePasswordを実装予定です。

--- dao/UserDao.java
+++ dao/UserDao.java
@@ -21,7 +21,7 @@ public class UserDao extends Dao{
 	 * @return ArrayList<UserDto> 退職フラグがoffな従業員一覧、ただし例外発生によって正常に返却しないリスクあり
 	 * @throws SQLException
 	 */
-	public ArrayList<UserDto> findAll() throws SQLException{
+	public ArrayList<UserDto> findAll() throws SQLException{ // メソッド項番1
 		String sql = "SELECT * FROM user WHERE retiredFlag = FALSE;";
 		
 		ArrayList<UserDto> list = new ArrayList<UserDto>(); // resultで返却されるべきリスト
@@ -48,15 +48,15 @@ public class UserDao extends Dao{
 		
 		return list;
 	}
-	/**
-	 * 
+	/** メソッド項番2
+	 * パスワード確認などのため、userIDをもとに情報を取得します。
 	 * @param userId(文字列型)
-	 * @return
+	 * @return UserDto
 	 */
 	public UserDto selectById (String userId) {
 		String sql = "SELECT * FROM user WHERE userId = ?;" ; // 準備中のSQL
 		
-		UserDto arg = new UserDto(); // 返却されるべきUserDtoインスタンス
+		UserDto arg = new UserDto(); // @param 返却されるべきUserDtoインスタンス
 		
 		try {
 			PreparedStatement ps = connection.prepareStatement(sql);
@@ -76,7 +76,36 @@ public class UserDao extends Dao{
 		}
 		return arg;
 	}
+	/**  メソッド項番3
+	 * 管理者画面でボタンをクリックされた際、
+	 * 固定となる仮パスワードを発行するためのメソッドです。
+	 * および、LoginControllerでの処理準備のため、passFlag(仮パスフラグ)をTRUEにします。
+	 * @param String userId
+	 * @return 変更されたレコード数（通常なら１、userIdが見つからず変更されない場合0）
+	 * @throws SQLException
+	 */
+	public int setKariPassword (String userId) throws SQLException {
+		
+		String sql = "UPDATE user SET passFlag = true, pass = 'Pass2506'"
+				+ "WHERE userid = ? ;" ; // 準備中のSQL
+		// WHERE文で主キーを指定することで1つのみ更新予定
+		
+		int result = 0;
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, userId); // 引数を現在準備中のSQL文に入れ込んだ。
+			result = ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
 	
-	
+//	public int updatePassword () {
+//		
+//	}
 
 }
