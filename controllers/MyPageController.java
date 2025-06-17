ab9e86ffd9c563c34c883251a7a990093fefa988
RisaHiramatsu <risa.hiramatsu@akkodis.co.jp>
2025-06-17 15:31:05 +0900

    Add files via upload

---
+++ controllers/MyPageController.java
@@ -0,0 +1,57 @@
+package jp.co.akkodis.syumix
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Servlet implementation class MyPageController
+ */
+@WebServlet("/mypage")
+public class MyPageController extends HttpServlet {
+	
+	//投稿内容を取得
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+	ServletException, IOException {
+    	postDao postDao = new postDao();
+    	ArrayList userPosts = postDao.selectByUser(request.getSession().getAttribute("userId"));
+    	
+    	//JSPに渡す
+		request.setAttribute("userPosts", userPosts);
+		request.getRequestDispatcher("/mypage.jsp").forward(request, response);
+
+		
+	}
+    
+    //投稿を削除
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+	ServletException, IOException {
+		String action = request.getParameter("action");
+		boolean flug = true;
+		
+		if ("delete".equals(action)) { //削除ボタンを押した場合
+			int postID = Integer.parseInt(request.getParameter("postId"));
+	        postDao postDao = new postDao();
+	        postDao.delete(postID);
+	        request.setAttribute("deleteflug", flug);
+	        
+	        request.getRequestDispatcher("/mypage.jsp").forward(request, response);
+	        
+	    } else if ("main".equals(action)) { // メインページへボタンを押した場合
+	        request.getRequestDispatcher("/main.jsp").forward(request, response);
+	    }
+	        
+	        
+			
+		}
+				
+	}
+
+
