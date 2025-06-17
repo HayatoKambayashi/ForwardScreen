2122d0252217c84edf5e05165dd31894e18ad548
RisaHiramatsu <risa.hiramatsu@akkodis.co.jp>
2025-06-17 17:14:57 +0900

    Update PostDao.java

--- dao/PostDao.java
+++ dao/PostDao.java
@@ -98,13 +98,36 @@ public class PostDao extends Dao{
 	
 	// TODO: 以下1メソッドを要定義
 	public ArrayList<PostDto> selectByUser(String userId) {
+		String sql = "SELECT * FROM post WHERE userId = ? order by userId";
 		
-		return new ArrayList<>();
+		ArrayList<PostDto> postlist = new ArrayList<PostDto>();
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, userId);
+			// todo
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				PostDto post = new PostDto();
+				post.setPostId(rs.getInt(1));
+				post.setUserId(rs.getInt(2));
+				post.setSource(rs.getString(3));
+				post.setUrl(rs.getString(4));
+				post.setGenreCd(rs.getString(5));
+				post.setImage(rs.getString(6));
+				post.setAnonyFlag(rs.getBoolean(7));
+				post.setDate(rs.getDate(8));
+				
+				postlist.add(post); // TODO: 実装途中です。妥当性検証中
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return postlist;
 	}
-	
-//	public PostDto pick () { // 担当： 矢島
-//		
-//	}
 	
 	//TODO: 以下2メソッドを要定義
 	public ArrayList<GenreDto> getAllGenre () {
