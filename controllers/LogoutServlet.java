d2639cd93565bb6eda113b91f55113b273ef885b
kanbaara-1211 <akkodis_2910727@i.softbank.jp>
2025-06-23 12:42:26 +0900

    Add files via upload

---
+++ controllers/LogoutServlet.java
@@ -0,0 +1,22 @@
+package jp.co.akkodis.syumix;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate(); // セッションを破棄
+        }
+        response.sendRedirect("login.jsp"); // ログイン画面にリダイレクト
+    }
+}
