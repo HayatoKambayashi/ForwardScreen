de8fffcf15723303a1be88366b1f56783390bf2c
92fumi <fu.yajima@kanetsu-sw.co.jp>
2025-06-23 16:23:38 +0900

    Update ManagerController.java

--- controllers/ManagerController.java
+++ controllers/ManagerController.java
@@ -29,7 +29,7 @@ public class ManagerController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		// セッションチェック
-		HttpSession session = request.getSession();
+		HttpSession session = request.getSession(false);
 		if (session == null || session.getAttribute("manager") == null) {
 			if (session.getAttribute("loginUser") == null) {
 				// ログインユーザ情報がない場合。
